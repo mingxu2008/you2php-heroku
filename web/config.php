67e631275ed8b75c29a3b536397f86be71ce95ea
mingxu2008 <31261515+mingxu2008@users.noreply.github.com>
2018-04-24 16:43:02 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
+define('APIKEY', 'AIzaSyB_cBqA6qQXP76gtyqcAD7vLPv_c9IwWYg');
 define('GJ_CODE', getenv("GJ_CODE"));
 define('SITE_NAME',getenv("SITE_NAME"));
 define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('EN2DEKEY','xpy044000');
+define('EMAIL', 'mingxu2008yeah@gmail.com');
 ?>
